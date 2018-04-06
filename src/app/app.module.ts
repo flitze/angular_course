3a6e357bd5db5f341172be3b64be166cf7f0cf83
Rafael Hochreuter <rafoe@hotmail.de>
2018-04-06 19:36:51 +0200

    Angular component 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {  MatButtonModule, 
+          MatCheckboxModule, 
+          MatDatepickerModule, 
+          MatFormFieldModule,
+          MatInputModule, 
+          MatRadioModule, 
+          MatSelectModule, 
+          MatSliderModule,
+          MatSlideToggleModule, 
+          MatToolbarModule, 
+          MatListModule, 
+          MatGridListModule,
+          MatCardModule, 
+          MatIconModule, 
+          MatProgressSpinnerModule, 
+          MatDialogModule } from '@angular/material'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
 
-
+import 'hammerjs';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule, 
+    MatCheckboxModule, 
+    MatDatepickerModule, 
+    MatFormFieldModule,
+    MatInputModule, 
+    MatRadioModule, 
+    MatSelectModule, 
+    MatSliderModule,
+    MatSlideToggleModule, 
+    MatToolbarModule, 
+    MatListModule, 
+    MatGridListModule,
+    MatCardModule, 
+    MatIconModule, 
+    MatProgressSpinnerModule, 
+    MatDialogModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
