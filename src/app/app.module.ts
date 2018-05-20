b6423ca06b9b4d20ee9806387131bda1b11f7b2a
Rafael Hochreuter <rafoe@hotmail.de>
2018-05-20 15:49:34 +0200

    Directives.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,6 +44,7 @@ import { baseURL } from './shared/baseurl';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 import { RestangularModule, Restangular } from 'ngx-restangular';
 import { RestangularConfigFactory } from './shared/restConfig';
+import { HighlightDirective } from './directives/highlight.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -54,7 +55,8 @@ import { RestangularConfigFactory } from './shared/restConfig';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
